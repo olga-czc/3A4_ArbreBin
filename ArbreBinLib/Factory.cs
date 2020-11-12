20cee23d223423c3ea51236f9746cc1ca74d8ff2
Olga Cazacioc <1697327@cstjean.qc.ca>
2020-11-11 22:45:16 -0500

    Factory

--- ArbreBinLib/Factory.cs
+++ ArbreBinLib/Factory.cs
@@ -7,10 +7,42 @@ namespace ArbreBinLib
 {
     public static partial class Factory
     {
-        public static IEnumerable<Noeud?> ArbreNull() => throw new NotImplementedException();
-        public static IEnumerable<Noeud?> Arbres1() => throw new NotImplementedException();
-        public static IEnumerable<Noeud?> Arbres2() => throw new NotImplementedException();
-        public static IEnumerable<Noeud?> Arbres3() => throw new NotImplementedException();
-        public static IEnumerable<Noeud?> ArbresTypiques() => throw new NotImplementedException();
+        public static IEnumerable<Noeud?> ArbreNull()
+        {
+            yield return null;
+
+        }
+        public static IEnumerable<Noeud?> Arbres1()
+        {
+            yield return new Noeud(1);
+            yield return new Noeud(2);
+        }
+        public static IEnumerable<Noeud?> Arbres2()
+        {
+            yield return new Noeud(2, gauche: new Noeud(1));
+            yield return new Noeud(1, droite: new Noeud(2));
+        }
+        public static IEnumerable<Noeud?> Arbres3()
+        {
+            yield return new Noeud(3, gauche: new Noeud(2, gauche : new Noeud(1)));
+            yield return new Noeud(3, gauche: new Noeud(1, droite: new Noeud(2)));
+            yield return new Noeud(2, gauche: new Noeud(1), droite: new Noeud(3));
+            yield return new Noeud(1, droite: new Noeud(3, gauche: new Noeud(2)));
+            yield return new Noeud(1, droite: new Noeud(2, droite: new Noeud(3)));
+        }
+        public static IEnumerable<Noeud?> ArbresTypiques()
+        {
+            {
+                yield return new Noeud(3, gauche: new Noeud(2, gauche: new Noeud(1)), droite: new Noeud(4, droite: new Noeud(5)));
+                yield return new Noeud(6, droite: new Noeud(3, gauche: new Noeud(4, droite: new Noeud(1)), droite: new Noeud(2, gauche: new Noeud(5))));
+                yield return new Noeud(20, droite: new Noeud(25, gauche: new Noeud(21), droite: new Noeud(28)), gauche: new Noeud(5, gauche: new Noeud(3),
+                    droite: new Noeud(12, droite: new Noeud(13), gauche: new Noeud(8, gauche: new Noeud(6)))));
+                yield return new Noeud(15, droite: new Noeud(20, gauche: new Noeud(16, droite: new Noeud(17)), droite: new Noeud(21)), gauche: new Noeud(12, gauche: new Noeud(8, droite: new Noeud(10)),
+                   droite: new Noeud(14, gauche: new Noeud(13))));
+                yield return new Noeud(1, droite: new Noeud(3, droite: new Noeud(6, gauche: new Noeud(9))), gauche: new Noeud(2, gauche: new Noeud(4), droite: new Noeud(5, droite: new Noeud(8), gauche: new Noeud(7))));
+
+
+            }
+        }
     }
 }
